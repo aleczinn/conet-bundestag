db8a2ac8ae1f9f6cd6de81394a85a1173ad3d5c0
Alec <alec_z17@web.de>
2026-03-25 13:59:29 +0100

    added logo icons

--- src/components/icons/index.ts
+++ src/components/icons/index.ts
@@ -1,3 +1,5 @@
+export { default as IconBundestagDesktop } from "./IconBundestagDesktop";
+export { default as IconBundestagMobile } from "./IconBundestagMobile";
 export { default as IconEasy } from "./IconEasy";
 export { default as IconGlobe } from "./IconGlobe";
 export { default as IconSign } from "./IconSign";
