edf9a814b9ab86815df5936a3f0dc1247517f788
Alec <alec_z17@web.de>
2026-03-31 14:21:48 +0200

    fixed sitemap to contain all locales

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,18 +1,29 @@
 import { MetadataRoute } from 'next';
 import { BASE_URL } from '@/lib/site';
 import { getLinks } from '@/lib/storyblok-queries';
+import { localeKeys, localeMap } from '@/lib/locale/locales';
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
 	const { data } = await getLinks();
 
-	console.log("sitemap");
-
 	return Object.values(data.links)
 		.filter((link: any) => !link.is_folder)
-		.map((link: any) => ({
-			url: `${BASE_URL}/${link.slug === 'home' ? '' : link.slug}`,
-			lastModified: link.published_at,
-			changeFrequency: 'weekly' as const,
-			priority: link.slug === 'home' ? 1 : 0.8,
-		}));
+		.flatMap((link: any) => {
+			const path = link.slug === 'home' ? '' : link.slug;
+
+			return localeKeys.map((key) => ({
+				url: `${BASE_URL}/${localeMap[key].urlSegment}/${path}`.replace(/\/+$/, ''),
+				lastModified: link.published_at,
+				changeFrequency: 'weekly' as const,
+				priority: link.slug === 'home' ? 1 : 0.8,
+				alternates: {
+					languages: Object.fromEntries(
+						localeKeys.map((k) => [
+							localeMap[k].urlSegment,
+							`${BASE_URL}/${localeMap[k].urlSegment}/${path}`.replace(/\/+$/, ''),
+						])
+					),
+				},
+			}));
+		});
 }
