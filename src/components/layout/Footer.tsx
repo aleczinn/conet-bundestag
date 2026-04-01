5aa0b9f95e730c875ba03c540f1253f5a80f4627
Alec <alec_z17@web.de>
2026-04-01 13:52:35 +0200

    implement footer

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,7 +1,84 @@
+import Container from '@/components/layout/Container';
+import { IconInstagram, IconLinkedIn, IconWhatsApp, IconYouTube } from '@/components/icons';
+import IconMastodon from '@/components/icons/IconMastodon';
+
 export default function Footer() {
 	const currentYear = new Date().getFullYear();
 
 	return (
-		<footer>All rights reserved © {currentYear} </footer>
+		<footer className="pb-24 w-full bg-white">
+			<Container className="py-8">
+				<div className="flex flex-col items-center md:flex-row justify-between gap-y-8 mb-4">
+					<ul className="flex flex-row items-center gap-4 order-1">
+						<li>
+							<a className="bt-link-bold" title="Hilfe" href="#">Hilfe</a>
+						</li>
+
+						<li>
+							<a className="bt-link-bold" title="Kontakt" href="#">Kontakt</a>
+						</li>
+
+						<li>
+							<a className="bt-link-bold" title="Inhaltsübersicht" href="#">Inhaltsübersicht</a>
+						</li>
+					</ul>
+
+					<ul className="flex flex-row items-center gap-8 font-bold md:order-2">
+						<li>
+							<button title="Instagram" className="text-sm flex flex-row items-center gap-2">
+								<IconInstagram className="w-8 h-8" />
+								<span className="hidden lg:block">Instagram</span>
+							</button>
+						</li>
+
+						<li>
+							<button title="LinkedIn" className="text-sm flex flex-row items-center gap-2">
+								<IconLinkedIn className="w-8 h-8" />
+								<span className="hidden lg:block">LinkedIn</span>
+							</button>
+						</li>
+
+						<li>
+							<button title="Mastodon" className="text-sm flex flex-row items-center gap-2">
+								<IconMastodon className=" w-8 h-8" />
+								<span className="hidden lg:block">Mastodon</span>
+							</button>
+						</li>
+
+						<li>
+							<button title="WhatsApp" className="text-sm flex flex-row items-center gap-2">
+								<IconWhatsApp className="w-8 h-8" />
+								<span className="hidden lg:block">WhatsApp</span>
+							</button>
+						</li>
+
+						<li>
+							<button title="YouTube" className="text-sm flex flex-row items-center gap-2">
+								<IconYouTube className="w-8 h-8" />
+								<span className="hidden lg:block">YouTube</span>
+							</button>
+						</li>
+					</ul>
+				</div>
+
+				<div className="flex flex-col md:flex-row justify-between gap-y-3.5">
+					<div className="text-sm order-1">© {currentYear} | Deutscher Bundestag</div>
+
+					<ul className="flex flex-row items-center gap-4 md:order-2">
+						<li>
+							<a className="bt-link" title="Barrierefreiheit" href="#">Barrierefreiheit</a>
+						</li>
+
+						<li>
+							<a className="bt-link" title="Datenschutz" href="#">Datenschutz</a>
+						</li>
+
+						<li>
+							<a className="bt-link" title="Impressum" href="#">Impressum</a>
+						</li>
+					</ul>
+				</div>
+			</Container>
+		</footer>
 	);
 }
