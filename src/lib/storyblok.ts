f55314af8429714981985657359d375a2e9bf026
Alec <alec_z17@web.de>
2026-04-13 10:21:46 +0200

    remove test components

--- src/lib/storyblok.ts
+++ src/lib/storyblok.ts
@@ -1,7 +1,5 @@
 import { apiPlugin, storyblokInit } from '@storyblok/react/rsc';
 import Page from '@/components/blocks/Page';
-import Feature from '@/components/blocks/Feature';
-import Grid from '@/components/blocks/Grid';
 import Teaser from '@/components/blocks/Teaser';
 import Hero from '@/components/blocks/Hero';
 import MediaWithText from '@/components/blocks/MediaWithText';
@@ -21,11 +19,9 @@ export const getStoryblokApi = storyblokInit({
 	use: [apiPlugin],
 	components: {
 		page: Page,
-		feature: Feature,
-		grid: Grid,
-		teaser: Teaser,
 		hero: Hero,
 		media_with_text: MediaWithText,
+		teaser: Teaser,
 	},
 	apiOptions: {
 		/** Set the correct region for your space. Learn more: https://www.storyblok.com/docs/packages/storyblok-js#example-region-parameter */
