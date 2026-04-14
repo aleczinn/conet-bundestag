107886a3615bb837a03ff6c609a9e1bb64a68189
Alec <alec_z17@web.de>
2026-04-14 14:10:27 +0200

    temp comment out

--- src/app/llms.txt/route.ts
+++ src/app/llms.txt/route.ts
@@ -5,27 +5,29 @@ import { DEFAULT_LOCALE } from '@/lib/locale/locales';
 export const revalidate = 3600;
 
 export async function GET() {
-	const { data } = await getLinks(undefined, true);
-	const links = Object.values(data.links ?? {})
-		.filter((l: any) => !l.is_folder && !l.slug?.startsWith('config'))
-		.map((l: any) => {
-			const path = l.slug === 'home' ? '' : l.slug;
-			return `- [${l.name}](${BASE_URL}/${DEFAULT_LOCALE.language}/${path})`;
-		})
-		.join('\n');
+// 	const { data } = await getLinks();
+// 	const links = Object.values(data.links ?? {})
+// 		.filter((l: any) => !l.is_folder && !l.slug?.startsWith('config'))
+// 		.map((l: any) => {
+// 			const path = l.slug === 'home' ? '' : l.slug;
+// 			return `- [${l.name}](${BASE_URL}/${DEFAULT_LOCALE.language}/${path})`;
+// 		})
+// 		.join('\n');
+//
+// 	const content = `# ${SITE_NAME}
+//
+// > ${SITE_DESCRIPTION ?? ''}
+//
+// ## Seiten
+//
+// ${links}
+//
+// ## Weitere Ressourcen
+//
+// - [Sitemap](${BASE_URL}/sitemap.xml)
+// `;
 
-	const content = `# ${SITE_NAME}
-
-> ${SITE_DESCRIPTION ?? ''}
-
-## Seiten
-
-${links}
-
-## Weitere Ressourcen
-
-- [Sitemap](${BASE_URL}/sitemap.xml)
-`;
+	const content = ``;
 
 	return new Response(content, {
 		headers: { 'Content-Type': 'text/plain; charset=utf-8' },
