077c5dfcf11d6941e321f3581f774f5d1f2e5ef0
Alec <alec_z17@web.de>
2026-04-15 15:02:57 +0200

    adjust order (headlines, text)

--- src/app/fonts.ts
+++ src/app/fonts.ts
@@ -1,17 +1,5 @@
 import localFont from 'next/font/local';
 
-export const notoSans = localFont({
-	src: [
-		{ path: '../../public/fonts/noto-sans-display-v30-latin_latin-ext-regular.woff2', weight: '400', style: 'normal' },
-		{ path: '../../public/fonts/noto-sans-display-v30-latin_latin-ext-700.woff2', weight: '700', style: 'normal' },
-	],
-	variable: '--font-sans-noto',
-	display: 'swap',
-	preload: true,
-	fallback: ['Arial', 'Helvetica', 'sans-serif'],
-	adjustFontFallback: 'Arial',
-});
-
 export const notoSerif = localFont({
 	src: [
 		{ path: '../../public/fonts/noto-serif-v33-latin_latin-ext-regular.woff2', weight: '400', style: 'normal' },
@@ -24,4 +12,16 @@ export const notoSerif = localFont({
 	preload: true,
 	fallback: ['Georgia', 'Times New Roman', 'serif'],
 	adjustFontFallback: 'Times New Roman',
+});
+
+export const notoSans = localFont({
+	src: [
+		{ path: '../../public/fonts/noto-sans-display-v30-latin_latin-ext-regular.woff2', weight: '400', style: 'normal' },
+		{ path: '../../public/fonts/noto-sans-display-v30-latin_latin-ext-700.woff2', weight: '700', style: 'normal' },
+	],
+	variable: '--font-sans-noto',
+	display: 'swap',
+	preload: true,
+	fallback: ['Arial', 'Helvetica', 'sans-serif'],
+	adjustFontFallback: 'Arial',
 });
